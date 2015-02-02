96c500577007ac8bb7f3c79b257e4ca648d25784
Joseph Cohen <joseph.cohen@dinkbit.com>
2015-02-02 16:56:18 -0600

    Updated to Laravel Elixir 0.10.6

--- index.js
+++ index.js
@@ -1,50 +1,39 @@
 var gulp = require('gulp'),
-    to5 = require('gulp-6to5'),
-    notify = require('gulp-notify');
+    6to5 = require('gulp-6to5'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
+    _ = require('underscore'),
     elixir = require('laravel-elixir'),
-     _ = require('underscore'),
-    utilities = require('laravel-elixir/ingredients/helpers/utilities');
+    utilities = require('laravel-elixir/ingredients/commands/Utilities'),
+    notification = require('laravel-elixir/ingredients/commands/Notification');
 
 elixir.extend('6to5', function (src, outputDir, options) {
 
     var config = this,
-        baseDir = config.assetsDir + 'js',
-        defaultOptions;
-
-    defaultOptions = {
-    };
-
-    src = utilities.buildGulpSrc(src, baseDir, '**/*.js');
+        defaultOptions = {
+            debug:         ! config.production,
+            srcDir:        config.assetsDir + 'js',
+            output:        config.jsOutput
+        };
 
     options = _.extend(defaultOptions, options);
+    src = "./" + utilities.buildGulpSrc(src, options.srcDir);
 
     gulp.task('6to5', function () {
-        var onError = function(err) {
-            notify.onError({
-                title:    "Laravel Elixir",
-                subtitle: "6to5 Compilation Failed!",
-                message:  "Error: <%= error.message %>",
-                icon: __dirname + '/../laravel-elixir/icons/fail.png'
-            })(err);
 
+        var onError = function(e) {
+            new notification().error(e, '6to5 Failed!');
             this.emit('end');
         };
 
         return gulp.src(src)
             .pipe(6to5(options)).on('error', onError)
-            .pipe(gulpif(config.production, uglify()))
-            .pipe(gulp.dest(outputDir || config.jsOutput))
-            .pipe(notify({
-                title: 'Laravel Elixir',
-                subtitle: '6to5 Compiled!',
-                icon: __dirname + '/../laravel-elixir/icons/laravel.png',
-                message: ' '
-            }));
+            .pipe(gulpIf(! options.debug, uglify()))
+            .pipe(gulp.dest(options.output))
+            .pipe(new notification().message('6to5 Compiled!'));
     });
 
-    this.registerWatcher('6to5', baseDir + '/**/*.js');
+    this.registerWatcher('6to5', options.srcDir + '/**/*.js');
 
     return this.queueTask('6to5');
 
